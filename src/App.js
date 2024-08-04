e694d0c8af46636a6dad129db190b5b39d8e3e9f
Maxiflexy <onyemax247@gmail.com>
2024-08-05 00:58:03 +0100

    created components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import api from "./api/axiosConfig";
 import { useState, useEffect } from "react";
 import "./App.css";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -19,9 +21,31 @@ function App() {
 
   useEffect(() => {
     getMovies();
-  },[])
+  }, []);
 
-  return <div className="App"><h1>Hello</h1></div>;
+  return (
+    <div className="App">
+      {/* <Header/> */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                movie={movie}
+                reviews={reviews}
+                setReviews={setReviews}
+              />
+            }
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
