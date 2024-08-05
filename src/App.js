761b605a48cfa6853f4fce5badd753c2e3491864
Maxiflexy <onyemax247@gmail.com>
2024-08-05 10:36:57 +0100

    created Trailer.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 import Layout from "./components/Layout";
 import { Routes, Route } from "react-router-dom";
 import Home from './components/home/Home';
-// import Header from './components/header/Header';
-// import Trailer from './components/trailer/Trailer';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
 // import Reviews from './components/reviews/Reviews';
 // import NotFound from './components/notFound/NotFound';
 
@@ -34,7 +34,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />}></Route>
-          {/* <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
           <Route
             path="/Reviews/:movieId"
             element={
